888a77751ae663b77d6b2780d4405db7599d99f9
ujicrush <evangelinezhang523@gmail.com>
2025-12-06 12:55:45 +0100

    z subsystem validated

---
+++ project/LinearMPC/MPCControl_zvel.py
@@ -0,0 +1,77 @@
+import cvxpy as cp
+import numpy as np
+from control import dlqr
+
+from .MPCControl_base import MPCControl_base
+
+
+class MPCControl_zvel(MPCControl_base):
+    x_ids: np.ndarray = np.array([8])
+    u_ids: np.ndarray = np.array([2])
+
+    # only useful for part 5 of the project
+    d_estimate: np.ndarray
+    d_gain: float
+
+    def _setup_controller(self) -> None:
+        #################################################
+        # YOUR CODE HERE
+        super()._setup_controller()
+
+        A, B = self.A, self.B
+        nx, nu, N = self.nx, self.nu, self.N
+
+        Q = 60 * np.eye(nx)
+        R = 35 * np.eye(nu)
+
+        self.Q = Q
+        self.R = R
+
+        K, P, _ = dlqr(A, B, Q, R)
+        self.K = -K
+        self.P = P
+
+        self.constraints +=[
+            self.U <= 80-self.us[0],
+            self.U >= 40-self.us[0]
+        ]
+
+        self.objective = 0
+        for k in range(self.N):
+            self.objective += cp.quad_form(self.X[:, k] - self.x_ref_param, Q)
+            self.objective += cp.quad_form(self.U[:, k] - self.u_ref_param, R)
+        self.objective += cp.quad_form(self.X[:, self.N] - self.x_ref_param, P)
+
+        self.ocp = cp.Problem(cp.Minimize(self.objective), self.constraints)
+        # YOUR CODE HERE
+        #################################################
+
+    def get_u(
+        self, x0: np.ndarray, x_target: np.ndarray = None, u_target: np.ndarray = None
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        #################################################
+        # YOUR CODE HERE
+        u0, x_traj, u_traj = super().get_u(x0, x_target, u_target)
+        # YOUR CODE HERE
+        #################################################
+
+        return u0, x_traj, u_traj
+
+    def setup_estimator(self):
+        # FOR PART 5 OF THE PROJECT
+        ##################################################
+        # YOUR CODE HERE
+
+        self.d_estimate = ...
+        self.d_gain = ...
+
+        # YOUR CODE HERE
+        ##################################################
+
+    def update_estimator(self, x_data: np.ndarray, u_data: np.ndarray) -> None:
+        # FOR PART 5 OF THE PROJECT
+        ##################################################
+        # YOUR CODE HERE
+        self.d_estimate = ...
+        # YOUR CODE HERE
+        ##################################################

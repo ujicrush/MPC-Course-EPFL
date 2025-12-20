e45a82b9c58e33f5a408533e93adc161ea51f347
Shreesh Mahapatra <shreesh.mahapatra@epfl.ch>
2025-12-20 16:49:34 +0100

    Add LinearMPC velocity controllers for Deliverable 5.1

---
+++ project/Deliverable_5_1/LinearMPC/.ipynb_checkpoints/MPCControl_base-checkpoint.py
@@ -0,0 +1,116 @@
+import cvxpy as cp
+import numpy as np
+from control import dlqr
+from mpt4py import Polyhedron
+from scipy.signal import cont2discrete
+
+
+class MPCControl_base:
+    """Complete states indices"""
+
+    x_ids: np.ndarray
+    u_ids: np.ndarray
+
+    """Optimization system"""
+    A: np.ndarray
+    B: np.ndarray
+    xs: np.ndarray
+    us: np.ndarray
+    nx: int
+    nu: int
+    Ts: float
+    H: float
+    N: int
+
+    """Optimization problem"""
+    ocp: cp.Problem
+
+    def __init__(
+        self,
+        A: np.ndarray,
+        B: np.ndarray,
+        xs: np.ndarray,
+        us: np.ndarray,
+        Ts: float,
+        H: float,
+    ) -> None:
+        self.Ts = Ts
+        self.H = H
+        self.N = int(H / Ts)
+        self.nx = self.x_ids.shape[0]
+        self.nu = self.u_ids.shape[0]
+
+        # System definition
+        xids_xi, xids_xj = np.meshgrid(self.x_ids, self.x_ids)
+        A_red = A[xids_xi, xids_xj].T
+        uids_xi, uids_xj = np.meshgrid(self.x_ids, self.u_ids)
+        B_red = B[uids_xi, uids_xj].T
+
+        self.A, self.B = self._discretize(A_red, B_red, Ts)
+        self.xs = xs[self.x_ids]
+        self.us = us[self.u_ids]
+
+        self._setup_controller()
+
+    def _setup_controller(self) -> None:
+        #################################################
+        # YOUR CODE HERE
+        nx, nu = self.nx, self.nu
+        N = self.N
+        A, B = self.A, self.B
+
+        self.X = cp.Variable((nx, N + 1), name="X")
+        self.U = cp.Variable((nu, N), name="U")
+
+        self.x0_param = cp.Parameter(nx, name="x0")
+
+        constraints = []
+        constraints.append(self.X[:, 0] == self.x0_param)
+
+        for k in range(N):
+            constraints.append(self.X[:, k + 1] == A @ self.X[:, k] + B @ self.U[:, k])
+
+        self.constraints = constraints
+        # YOUR CODE HERE
+        #################################################
+
+    @staticmethod
+    def _discretize(A: np.ndarray, B: np.ndarray, Ts: float):
+        nx, nu = B.shape
+        C = np.zeros((1, nx))
+        D = np.zeros((1, nu))
+        A_discrete, B_discrete, _, _, _ = cont2discrete(system=(A, B, C, D), dt=Ts)
+        return A_discrete, B_discrete
+
+    def get_u(
+        self, x0: np.ndarray, x_target: np.ndarray = None, u_target: np.ndarray = None
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        #################################################
+        # YOUR CODE HERE
+        if x0.shape[0] != self.nx:
+            x_sub = x0[self.x_ids]
+        else:
+            x_sub = x0
+
+        delta_x0 = x_sub - self.xs
+        self.x0_param.value = delta_x0
+
+        self.ocp.solve(solver=cp.GUROBI, warm_start=True)
+
+        if self.ocp.status != cp.OPTIMAL:
+            delta_u0 = np.zeros(self.nu)
+            delta_x_traj = np.tile(delta_x0.reshape(-1, 1), (1, self.N + 1))
+            delta_u_traj = np.zeros((self.nu, self.N))
+        else:
+            delta_u0 = self.U[:, 0].value
+            delta_x_traj = self.X.value
+            delta_u_traj = self.U.value
+
+        u0 = delta_u0 + self.us
+        u_traj = delta_u_traj + self.us.reshape(-1, 1)
+        x_traj = delta_x_traj + self.xs.reshape(-1, 1)
+
+        # YOUR CODE HERE
+        #################################################
+
+        return u0, x_traj, u_traj
---
+++ project/Deliverable_5_1/LinearMPC/.ipynb_checkpoints/MPCControl_xvel-checkpoint.py
@@ -0,0 +1,128 @@
+import cvxpy as cp
+import numpy as np
+from control import dlqr
+from mpt4py import Polyhedron
+import matplotlib.pyplot as plt
+
+from .MPCControl_base import MPCControl_base
+
+
+class MPCControl_xvel(MPCControl_base):
+    x_ids: np.ndarray = np.array([1, 4, 6])
+    u_ids: np.ndarray = np.array([1])
+
+    def _setup_controller(self) -> None:
+        #################################################
+        # YOUR CODE HERE
+        super()._setup_controller()
+
+        A, B = self.A, self.B
+        nx, nu, N = self.nx, self.nu, self.N
+
+        # # for deliverable 3.1-3.2
+        # Q = 0.01 * np.eye(nx)
+        # Q[0,0] *= 10
+        # Q[1,1] *= 80
+        # Q[2,2] *= 1
+        # R = 100 * np.eye(nu)
+
+        # # for deliverable 3.3
+        # Q = 0.01 * np.eye(nx)
+        # Q[0,0] *= 200
+        # Q[1,1] *= 80
+        # Q[2,2] *= 1
+        # R = 100 * np.eye(nu)
+
+        # for deliverable 4.1
+        Q = 0.01 * np.eye(nx)
+        Q[0,0] *= 7000
+        Q[1,1] *= 20
+        Q[2,2] *= 1
+        R = 10 * np.eye(nu)
+
+        self.Q = Q
+        self.R = R
+
+        K, Qf, _ = dlqr(A, B, Q, R)
+        self.K = -K
+        self.Qf = Qf
+
+        # x in X = { x | Fx <= f }
+        F_x = np.array([
+            [0.0, 1.0, 0.0],
+            [0.0,-1.0, 0.0],
+        ])
+        f_x = np.array([0.1745, 0.1745])
+
+        # u in U = { u | Mu <= m }
+        M_u = np.array([[1.0],
+                        [-1.0]])
+        m_u = np.array([0.26, 0.26])
+
+        F_term = np.vstack([F_x, M_u @ self.K])   # (4,3)
+        f_term = np.hstack([f_x, m_u])            # (4,)
+
+        Xf = Polyhedron.from_Hrep(F_term, f_term)
+        Acl = A + B @ self.K
+
+        while True:
+            Xf_prev = Xf
+            F_O, f_O = Xf.A, Xf.b
+            pre = Polyhedron.from_Hrep(F_O @ Acl, f_O)
+            Xf = Xf.intersect(pre)
+            Xf.minHrep(True)
+            _ = Xf.Vrep     # this is a tempary fix since the contains() method is not robust enough when both inner and outer polyhera only has H-rep.
+            if Xf == Xf_prev:
+                break
+
+        Ff, ff = Xf.A, Xf.b
+
+        # Visualization of terminal invariant set for x-subsystem
+        fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+
+        # Left: projection on (x0, x1)
+        proj01 = Xf.projection(dims=(0, 1))
+        proj01.plot(axes[0])
+        axes[0].set_title("Projection on (ω_y, β)")
+        axes[0].set_xlabel("ω_y")
+        axes[0].set_ylabel("β")
+
+        # Right: projection on (x1, x2)
+        proj12 = Xf.projection(dims=(1, 2))
+        proj12.plot(axes[1])
+        axes[1].set_title("Projection on (β, v_x)")
+        axes[1].set_xlabel("β")
+        axes[1].set_ylabel("v_x")
+
+        fig.suptitle("Terminal invariant set for x-subsystem", fontsize=14)
+        plt.tight_layout()
+        plt.show()
+
+        self.constraints += [
+            self.X[1, :] <= 0.1745,
+            self.X[1, :] >= -0.1745,
+            self.U <= 0.26,
+            self.U >= -0.26,
+            Ff @ self.X[:, self.N] <= ff
+        ]
+
+        self.objective = 0
+        for k in range(self.N):
+            self.objective += cp.quad_form(self.X[:, k], Q)
+            self.objective += cp.quad_form(self.U[:, k], R)
+        self.objective += cp.quad_form(self.X[:, N], Qf)
+
+        self.ocp = cp.Problem(cp.Minimize(self.objective), self.constraints)
+        # YOUR CODE HERE
+        #################################################
+
+    def get_u(
+        self, x0: np.ndarray, x_target: np.ndarray = None, u_target: np.ndarray = None
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        #################################################
+        # YOUR CODE HERE
+        u0, x_traj, u_traj = super().get_u(x0, x_target, u_target)
+        # YOUR CODE HERE
+        #################################################
+
+        return u0, x_traj, u_traj
